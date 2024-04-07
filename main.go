5ceee018512f1f93c62e4739cb993501b4e5c48a
FluffyFoxTail <fluffyfoxtail988@gmail.com>
2024-04-07 13:52:42 +0300

    Update main rayColor method to calculate refeclection by type of material

--- main.go
+++ main.go
@@ -23,10 +23,28 @@ var (
 
 	camera = rayTracer.NewCamera()
 
-	sphere = rayTracer.Sphere{Center: rayTracer.Vector{Z: -1}, Radius: 0.5}
-	floor  = rayTracer.Sphere{Center: rayTracer.Vector{Y: -100.5, Z: -1}, Radius: 100}
+	sphere = rayTracer.Sphere{
+		Center:   rayTracer.Vector{Z: -1},
+		Radius:   0.5,
+		Material: rayTracer.Lambertian{C: rayTracer.Vector{X: 0.8, Y: 0.3, Z: 0.3}},
+	}
+	floor = rayTracer.Sphere{
+		Center:   rayTracer.Vector{Y: -100.5, Z: -1},
+		Radius:   100,
+		Material: rayTracer.Lambertian{C: rayTracer.Vector{X: 0.8, Y: 0.8}},
+	}
+	leftSphere = rayTracer.Sphere{
+		Center:   rayTracer.Vector{X: -1, Z: -1},
+		Radius:   0.5,
+		Material: rayTracer.Metal{C: rayTracer.Vector{X: 0.8, Y: 0.8, Z: 0.8}, Fuzz: 0.0},
+	}
+	rightSphere = rayTracer.Sphere{
+		Center:   rayTracer.Vector{X: 1, Z: -1},
+		Radius:   0.5,
+		Material: rayTracer.Metal{C: rayTracer.Vector{X: 0.8, Y: 0.6, Z: 0.2}, Fuzz: 0.3},
+	}
 
-	world = rayTracer.World{Elems: []rayTracer.Hitable{&sphere, &floor}}
+	world = rayTracer.World{Elems: []rayTracer.Hittable{&sphere, &floor, &leftSphere, &rightSphere}}
 )
 
 func checkErr(e error, s string) {
@@ -35,28 +53,35 @@ func checkErr(e error, s string) {
 	}
 }
 
-func gradient(v *rayTracer.Vector) rayTracer.Vector {
+func writeColor(w io.Writer, v rayTracer.Vector) {
+	_, err := fmt.Fprintf(w, "%d %d %d\n", int(v.X*COLOR), int(v.Y*COLOR), int(v.Z*COLOR))
+	checkErr(err, "Error writing to file")
+}
+
+func gradient(r rayTracer.Ray) rayTracer.Vector {
+	// unit vector
+	unitDirection := r.Direction.Normalize()
+
 	// scale t to be between 0.0 and 1.0
-	t := 0.5 * (v.Y + 1.0)
+	t := 0.5 * (unitDirection.Y + 1.0)
 
 	// linear blend: blended_value = (1 - t) * white + t * blue
 	return white.MultiplyScalar(1.0 - t).Add(blue.MultiplyScalar(t))
 }
 
-func rayColor(r *rayTracer.Ray, h rayTracer.Hitable) rayTracer.Vector {
-	isHit, record := h.Hit(r, 0.0, math.MaxFloat64)
+func rayColor(r rayTracer.Ray, world rayTracer.Hittable, depth int) rayTracer.Vector {
+	isHit, record := world.Hit(r, 0.001, math.MaxFloat64)
 	if isHit {
-		return record.Normal.AddScalar(1.0).MultiplyScalar(0.5)
+		if depth < 50 {
+			isBounced, bouncedRay := record.Bounce(r, record)
+			if isBounced {
+				newColor := rayColor(bouncedRay, world, depth+1)
+				return record.Material.Color().Multiply(newColor)
+			}
+		}
+		return rayTracer.Vector{}
 	}
-
-	// make unit vector so y is between -1.0 and 1.0
-	unitDirection := r.Direction.Normalize()
-	return gradient(&unitDirection)
-}
-
-func writeColor(w io.Writer, v rayTracer.Vector) {
-	_, err := fmt.Fprintf(w, "%d %d %d\n", int(v.X*COLOR), int(v.Y*COLOR), int(v.Z*COLOR))
-	checkErr(err, "Error writing to file")
+	return gradient(r)
 }
 
 func main() {
@@ -84,7 +109,7 @@ func main() {
 				v := (float64(j) + rand.Float64()) / float64(imageHeight)
 
 				r := camera.RayAt(u, v)
-				color := rayColor(&r, &world)
+				color := rayColor(r, &world, 0)
 				rgb = rgb.Add(color)
 			}
 			// calc avg color
