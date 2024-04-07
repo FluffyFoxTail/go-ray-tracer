8c7c9043fcd8eaba96d6a57f25f073c1e21b1a87
FluffyFoxTail <fluffyfoxtail988@gmail.com>
2024-04-07 13:49:21 +0300

    Update method At in Ray struct

--- rayTracer/ray.go
+++ rayTracer/ray.go
@@ -4,9 +4,8 @@ type Ray struct {
 	Origin, Direction Vector
 }
 
-// At P(t)=A+tb
+// At Point(t)=A+tb
 func (r Ray) At(t float64) Vector {
 	b := r.Direction.MultiplyScalar(t)
-	a := r.Origin
-	return a.Add(b)
+	return r.Origin.Add(b)
 }
